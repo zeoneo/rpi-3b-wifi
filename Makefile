5fed45f613747fc6f21fcff8fc7dca31fa1e9737
zeo <zeo29021992@gmail.com>
2022-01-26 21:46:00 +0530

    tempo

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: wpa_supplicant.a
 	cd src && $(MAKE)
 
 wpa_supplicant.a:
-	git submodule update --init hostap
+	# git submodule update --init hostap
 	cd hostap/wpa_supplicant && make -f Makefile.rpi3b
 	cp hostap/wpa_supplicant/libwpa_supplicant.a src/
 
--- os_rpi3b.h
+++ os_rpi3b.h
@@ -13,22 +13,21 @@
 extern "C" {
 #endif
 
-#include <stdarg.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <plibc/string.h>
 #include <mem/kernel_alloc.h>
+#include <plibc/string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 typedef struct {
-	int x;
+    int x;
 } FILE;
 
 struct in_addr {
-	int x;
-} ;
+    int x;
+};
 
-
-typedef signed long	time_t;
+typedef signed long time_t;
 typedef unsigned int size_t;
 
 typedef uint8_t u8;
@@ -41,59 +40,38 @@ typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
 
-
 #include "os.h"
 
-
-#define __LITTLE_ENDIAN	1234
-#define __BYTE_ORDER	__LITTLE_ENDIAN
+#define __LITTLE_ENDIAN 1234
+#define __BYTE_ORDER    __LITTLE_ENDIAN
 #define WPA_TYPES_DEFINED
-
-
 
 #define __force
 
-#define bswap_16	bswap16
-#define bswap_32	bswap32
+#define bswap_16 bswap16
+#define bswap_32 bswap32
 
+int abs(int i);
 
+int isprint(int c);
 
+char* strrchr(const char* s, int c);
+char* strdup(const char* s);
 
-#define abs		__wpa_abs
-#define isprint		__wpa_isprint
-#define strrchr		__wpa_strrchr
-#define strdup		__wpa_strdup
-#define snprintf	__wpa_snprintf
-#define vsnprintf	__wpa_vsnprintf
-#define printf		__wpa_printf
-#define vprintf		__wpa_vprintf
-#define qsort		__wpa_qsort
-#define abort		__wpa_abort
+int snprintf(char* str, size_t size, const char* format, ...);
+int vsnprintf(char* str, size_t size, const char* format, va_list ap);
+int printf(const char* format, ...);
+int vprintf(const char* format, va_list ap);
 
-int abs (int i);
+void qsort(void* base, size_t nmemb, size_t size, int (*compare)(const void*, const void*));
 
-int isprint (int c);
+void abort(void);
 
-char *strrchr (const char *s, int c);
-char *strdup (const char *s);
-
-int snprintf (char *str, size_t size, const char *format, ...);
-int vsnprintf (char *str, size_t size, const char *format, va_list ap);
-int printf (const char *format, ...);
-int vprintf (const char *format, va_list ap);
-
-void qsort (void *base, size_t nmemb, size_t size,
-	    int (*compare) (const void *, const void *));
-
-void abort (void);
-
-void os_hexdump (const char *title, const void *p, size_t len);
+void os_hexdump(const char* title, const void* p, size_t len);
 
 // #define os_daemonize(s)			(0)
 
 // #define os_daemonize_terminate(s)	((void) 0)
-
-
 
 #ifdef __cplusplus
 }
